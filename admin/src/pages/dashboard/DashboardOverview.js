9f8aa234cc4521ce0397bd65b3bdce0d06ff973b
minhhoangzeno <minhhoangzeno@gmail.com>
2022-12-05 03:18:17 +0700

    commit code

--- admin/src/pages/dashboard/DashboardOverview.js
+++ admin/src/pages/dashboard/DashboardOverview.js
@@ -1,23 +1,24 @@
-import { Row } from '@themesberg/react-bootstrap';
+import { Row } from "@themesberg/react-bootstrap";
 import React from "react";
-import DashboardOrderByAmount from './DashboardOrderByAmount';
-import DashboardOrderByRevenue from './DashboardOrderByRevenue';
-
+import DashboardBlog from "./DashboardBlog";
+import DashboardOrder from "./DashboardOrder";
+import DashboardOrderByAmount from "./DashboardOrderByAmount";
+import DashboardOrderByRevenue from "./DashboardOrderByRevenue";
+import DashboardProduct from "./DashboardProduct";
+import DashboardUser from "./DashboardUser";
 
 export default () => {
   return (
     <>
       <Row className="justify-content-md-center">
-       
-
+        <DashboardUser />
+        <DashboardProduct />
+        <div></div>
+        <DashboardOrder />
+        <DashboardBlog />
         <DashboardOrderByAmount />
-
         <DashboardOrderByRevenue />
-
-       
       </Row>
-
-      
     </>
   );
 };
